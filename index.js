4a4a73f1ec5602be20f8065d24360973d948d7ac
Evgeny Proskurnikov <vesper182@inbox.ru>
2020-07-08 23:47:06 +0500

    Вторая редакция

--- index.js
+++ index.js
@@ -2,29 +2,31 @@ const openModalButton = document.querySelector('.profile__edit-button');
 const closeModalButton = document.querySelector('.modal__close-button');
 const modal = document.querySelector('.modal');
 
+let nameInput = modal.querySelector('.modal__input_type_name');
+let characterInput = modal.querySelector('.modal__input_type_character');
+let profileName = document.querySelector('.profile__name');
+let profileCharacter = document.querySelector('.profile__character');
+
+const formElement = modal.querySelector('.modal__form');
+
+
 function toggleModal () {
+  nameInput.value = profileName.textContent;
+  characterInput.value = profileCharacter.textContent;
   modal.classList.toggle('modal_opened');
+};
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileCharacter.textContent = characterInput.value;
+
+  toggleModal();
 };
 
 openModalButton.addEventListener('click', toggleModal);
 
 closeModalButton.addEventListener('click', toggleModal);
-
-
-const formElement = modal.querySelector('.modal__form');
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-
-  let nameInput = modal.querySelector('.modal__input_type_name');
-  let characterInput = modal.querySelector('.modal__input_type_character');
-  let nameOutput = document.querySelector('.profile__name');
-  let characterOutput = document.querySelector('.profile__character');
-
-  nameOutput.textContent = nameInput.value;
-  characterOutput.textContent = characterInput.value;
-
-  toggleModal();
-};
 
 formElement.addEventListener('submit', formSubmitHandler);
